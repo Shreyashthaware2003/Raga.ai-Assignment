9b329309ced93eae311503ab143138bddd4eaba8
Shreyash Thaware <shreyashthaware284@gmail.com>
2026-05-04 23:51:08 +0530

    minor changes

--- src/app/layout/Sidebar.tsx
+++ src/app/layout/Sidebar.tsx
@@ -83,6 +83,7 @@ export default function Sidebar({
 
     const handleLogout = async () => {
         await dispatch(logoutUser());
+        localStorage.clear();
         navigate("/login");
     };
 
